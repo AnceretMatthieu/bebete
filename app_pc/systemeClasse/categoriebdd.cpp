ba06bdc9315334c6cc2ebe24f1a0ec5cb15bdf1c
Martin <martin.demeule@mail.com>
2012-05-14 10:19:37 +0200

    maj

---
+++ app_pc/systemeClasse/categoriebdd.cpp
@@ -0,0 +1,50 @@
+#include "categoriebdd.h"
+
+ListeQuestion * CategorieBDD::CreerArbre()
+{
+    //QFile fichier(QDir::currentPath() + "/donnes_insectes.xml");
+    //QFile fichier(QDir::currentPath() + "/test.xml");
+    QFile fichier(QDir::currentPath() + "/accueil.xml");
+    QDomDocument doc;
+    doc.setContent(&fichier);
+    BDD::currentNode = doc.elementsByTagName("arbre").at(0);
+    qDebug() << currentNode.nodeName();
+
+    //firstchild = branche
+    BDD::currentNode = currentNode.firstChild();
+    qDebug() << currentNode.nodeName();
+
+    Categorie * cat = new Categorie(0);
+
+    listeQuestionWithCategorie(cat, true);
+
+    return cat->getListeQuestion();
+}
+
+void CategorieBDD::listeQuestionWithCategorie(Categorie * cat, bool recursif = true)   {
+
+    // récupération de la liste de toutes les questions
+    QDomNodeList lstBaliseQuestion = BDD::currentNode.childNodes(); // ==> renvoi "question"
+    qDebug() << "In listeQuestionWithCategorie(" << cat->getIdentifiant() << ", " << recursif << ")";
+    qDebug() << "nombre de question " << lstBaliseQuestion.size();
+
+    int i;
+    Question * temp;
+    for(i=0; i<lstBaliseQuestion.size(); i++)  {
+        qDebug() << "Question " << i;
+        //découpage du q devant l'id
+        temp = new Question(lstBaliseQuestion.at(i).toElement().attribute("id").left(1).toInt());
+        temp->setQuestion(lstBaliseQuestion.at(i).toElement().attribute("texte"));
+
+        qDebug() << "Question " << temp->getIdentifiant() << " : " << temp->getQuestion();
+
+        if(i > 0)   {
+             cat->getListeQuestion()->at(i-1)->setIdRight(temp->getIdentifiant());
+            temp->setIdLeft( cat->getListeQuestion()->at(i-1)->getIdentifiant());
+        }
+        BDD::currentNode = lstBaliseQuestion.at(i).toElement();
+        if(recursif)
+            QuestionBDD::listeReponseFromQuestion(temp, recursif);
+        cat->ajouterQuestion(temp);
+    }
+}
