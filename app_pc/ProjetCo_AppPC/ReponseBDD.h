f15842f6b67cf0c2dff25257949c0d432646f964
Martin <martin.demeule@mail.com>
2012-05-30 10:16:05 +0200

    Nettoyage des Qdebug and co

--- app_pc/ProjetCo_AppPC/ReponseBDD.h
+++ app_pc/ProjetCo_AppPC/ReponseBDD.h
@@ -13,9 +13,17 @@ class Reponse;
 class ListeReponse;
 class Question;
 
+/**
+ *\file ReponseBDD.h
+ *\class ReponseBDD
+ *\brief Classe permettant d'enregistrer et lire dans un fichier XML
+ */
+
 class ReponseBDD : public BDD {
 
     public:
+        /** \brief Constructeur par défaut de la classe ReponseBDD
+        */
         ReponseBDD();
         static void listeFromReponse(Reponse * rep, bool recursif);
         static void enregistrerReponse(Reponse * rep);
