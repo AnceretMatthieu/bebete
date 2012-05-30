51dde9c5996e5559a07c2a7a21e78c3dd59bb484
Heissler <heissler.jerome@gmail.com>
2012-05-30 11:12:29 +0200

    Mise en place de commentaire

--- app_pc/systemeClasse/BDD.h
+++ app_pc/systemeClasse/BDD.h
@@ -6,19 +6,33 @@
 #include <QFile>
 #include <QDebug>
 #include <QDir>
+#include "ListeQuestion.h"
+#include "Categorie.h"
 
 class BDD {
-
-    public:
-        BDD();
-
-        /*virtual bool insert();
-        virtual bool update();
-        virtual bool delete();*/
-
-        //QDomDocument currentNode;
-         static QDomNode currentNode;
+    protected:
+        /**
+         * \brief attribut mémorisant le noeud xml que l'on est en train de lire ou ecrire
+         */
+        static QDomNode currentNode;
+        /**
+         * \brief attribut mémorisant le document que l'on est en train de lire ou ecrire
+         */
          static QDomDocument doc;
+    public:
+        /**
+         *  \fn static ListeQuestion * CreerArbre()
+         *  \brief lit tout l'arbre dans le fichier XML
+         *  \return ListeQuestion * : la liste des questions à la racine de l'arborescence
+         */
+        static ListeQuestion * CreerArbre();
+
+        /**
+         *  \fn static void enregistrerArbre(Categorie *racine)
+         *  \brief enregistre le fichier xml dans un fichier passer en paramètre
+         *  \param Categorie *racine : la categorie en racine de l'arborescence
+         */
+        static void enregistrerArbre(Categorie *racine);
 };
 
 #endif // BDD_h
--- app_pc/systemeClasse/Categorie.h
+++ app_pc/systemeClasse/Categorie.h
@@ -10,6 +10,11 @@ using namespace std;
 class Question;
 class ListeQuestion;
 
+/**
+ *  \file Categorie.h
+ *  \class Categorie
+ *  \brief Class gérant une catégorie d'insecte, elle possède un nom, un identifiant unique et une liste de question permettant de trouver d'autre insecte
+ */
 class Categorie {
 
     private:
@@ -18,22 +23,53 @@ class Categorie {
         ListeQuestion * myQuestion;
 
     public:
+        /**
+         *  \fn Categorie(int identifiant)
+         *  \brief constructeur
+         */
         Categorie(int identifiant);
 
-        int getIdentifiant() {
-            return id;
-        }
+        /**
+         *  \fn int getIdentifiant()
+         *  \brief retourne l'identifiant de la catégorie
+         *  \return int
+         */
+        int getIdentifiant() { return id; }
 
-        QString getLabel() {
-            return label;
-        }
+        /**
+         *  \fn QString getLabel()
+         *  \brief retourne le nom de la catégorie
+         *  \return QString
+         */
+        QString getLabel() { return label; }
 
-        void setLabel(QString unLabel) {
-            label = unLabel;
-        }
+        /**
+         *  \fn void setLabel(QString unLabel)
+         *  \brief change le nom de la catégorie
+         *  \param QString unLabel : le nouveau nom de la catégorie
+         *  \return void
+         */
+        void setLabel(QString unLabel) { label = unLabel; }
 
+        /**
+         *  \fn void ajouterQuestion(Question * rep)
+         *  \brief Ajoute une Question dans la liste des questions
+         *  \param Question * rep : la question à ajouter
+         */
         void ajouterQuestion(Question * rep);
+
+        /**
+         *  \fn void supprimerQuestion(Question * rep)
+         *  \brief Supprime une Question dans la liste des questions
+         *  \param Question * rep : la question à supprimer
+         */
         void supprimerQuestion(Question * rep);
+
+        /**
+         *  \fn ListeQuestion * getListeQuestion()
+         *  \brief retourne la liste des questions de la catégorie
+         *  \return ListeQuestion *
+         */
         ListeQuestion * getListeQuestion()  {return myQuestion;}
 };
 
