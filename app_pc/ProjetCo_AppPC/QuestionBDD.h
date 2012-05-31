16820e72949cb44288b4bbfc9e406560a990aaf6
Martin <martin.demeule@mail.com>
2012-05-31 09:44:16 +0200

    maj doxygen

--- app_pc/ProjetCo_AppPC/QuestionBDD.h
+++ app_pc/ProjetCo_AppPC/QuestionBDD.h
@@ -15,10 +15,28 @@ class ListeQuestion;
 class ReponseBDD;
 class Categorie;
 
+/**
+ *  \class QuestionBDD
+ *  \brief Classe permettant de lire / √©crire une question dans un fichier XML, h√©rite de la classe BDD
+ *  \file questionbdd.h
+ */
 class QuestionBDD : public BDD {
 
     public:
+
+        /**
+         *  \fn static void listeReponseFromQuestion(Question *quest, bool recursif);
+         *  \brief lit les rÈponses du noeud en cours
+         *  \paramQuestion *quest : la question √  remplir
+         *  \param bool recursif : indique si l'on doit parcourrir les fils du noeud en cours
+         */
         static void listeReponseFromQuestion(Question * quest, bool recursif);
+
+        /**
+         *  \fn static void enregistrerQuestion(Question * quest)
+         *  \brief enregistre la question pass√© en param√®tre dans le noeud courrant
+         *  \param Question * quest : la question √  enregistrer
+         */
         static void enregistrerQuestion(Question * quest);
 };
 
--- app_pc/ProjetCo_AppPC/Reponse.h
+++ app_pc/ProjetCo_AppPC/Reponse.h
@@ -15,6 +15,11 @@ class Question;
 class Espece;
 class ListeMedia;
 
+/**
+ *  \class Reponse
+ *  \file Reponse.h
+ *  \brief classe gÃ©rant une réponse avec sa description
+ */
 class Reponse {
 
     private:
@@ -31,10 +36,28 @@ class Reponse {
         ListeMedia * illustrationMedia;
 
     public:
+
+        /**
+         * \fn Reponse(int id)
+         * \brief constructeur
+         * \param int id : l'identifiant unique de la réponse à instancier
+         */
         Reponse(int id);
 
+        /**
+         *  \fn int getIdentifiant()
+         *  \brief retourne l'identifiant de la réponse
+         *  \return int id
+         */
         int getIdentifiant() {  return id; }
+
+        /**
+         *  \fn int getReponse()
+         *  \brief retourne le texte de la réponse
+         *  \return QString texte
+         */
         QString getReponse() {  return texte;   }
+
 
         void setSuiv(void * suiv);
         void * getSuiv();
@@ -52,14 +75,42 @@ class Reponse {
         int getIdLeft()  {return idleft;}
         int getIdRight()  {return idright;}
 
+        /**
+         *  \fn void setReponse(QString rep)
+         *  \brief change le nom de la réponse
+         *  \param QString rep : le nouveau nom de la réponse
+         *  \return void
+         */
         void setReponse(QString rep) {
             texte = rep;
         }
 
+        /**
+         *  \fn ListeMedia * getListeIllustration()
+         *  \brief retourne la liste des illustrations du résultat
+         *  \return ListeMedia *
+         */
         ListeMedia * getListeIllustration() {return illustrationMedia;}
 
+        /**
+         *  \fn void ajouterMedia(Media * med);
+         *  \brief ajoute un mÃ©dia pour illustrer le résultat
+         *  \param Media * med : le nouveau mÃ©dia
+         */
         void ajouterMedia(Media * med);
+
+        /**
+         *  \fn void supprimerMedia(Media * med)
+         *  \brief supprime le mÃ©dia des résultats
+         *  \param Media * med : le mÃ©dia a supprimÃ©
+         */
         void supprimerMedia(Media * med);
+
+        /**
+         *  \fn void supprimerMedia(int id)
+         *  \brief supprime le mÃ©dia des résultats en fonction de son id
+         *  \param int id : l'identifiant du mÃ©dia a supprimÃ©
+         */
         void supprimerMedia(int id);
 };
 
